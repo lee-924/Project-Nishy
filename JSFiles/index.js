cbac15ff47f8e4922bba9db2edaf09715b3da573
lee <ahumada14344@gmail.com>
2026-04-21 23:22:32 +0300

    intro update

--- JSFiles/index.js
+++ JSFiles/index.js
@@ -1,3 +1,50 @@
+let codeBtn = document.querySelectorAll('.codeBtn')
+let input = '';
+let textArea = document.querySelector('.textArea')
+let hiddenDiv = document.querySelector('.hiddenDiv')
+
+const clickSound = new Audio('../sound/codeBtnSound.mp3')
+const confirmed = new Audio('../sound/confirmed.mp3')
+
+codeBtn.forEach(btn => {
+    btn.addEventListener('click',() => {
+        if (btn.dataset.value === 'Delete') {
+            textArea.value = ''
+            input = ''
+            textArea.style.color = 'white'
+        }
+        else if (btn.dataset.value === 'Enter') {
+            if (textArea.value === 'NISHY') {
+                textArea.value = 'CORRECT'
+                textArea.style.color = 'lime'
+                confirmed.play()
+                hiddenDiv.style.animationPlayState = 'running'
+                loading()
+            }
+
+        }
+        else {
+        input+= btn.dataset.value;
+        textArea.value = input
+        clickSound.currentTime = 0;
+        clickSound.play()
+    }
+    
+        
+    })
+})
+
+
+
+function loading() {
+    
+
+}
+
+
+
+
+
 let bx1 = document.querySelector('.box1')
 
 let b1 = document.querySelector('.b1')
